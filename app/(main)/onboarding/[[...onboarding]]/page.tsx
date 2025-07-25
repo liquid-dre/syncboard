6582e9a8da2209350b94461f9502cab0f9767cfc
liquid-dre <avdingiswayo54@gmail.com>
2025-07-25 14:32:19 +0200

    feat: Organisation creation

--- app/(main)/onboarding/[[...onboarding]]/page.tsx
+++ app/(main)/onboarding/[[...onboarding]]/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client"
+
+import { OrganizationList, useOrganization } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Onboarding = () => {
-  return (
-    <div>Onboarding</div>
-  )
-}
+	const { organization } = useOrganization();
+	const router = useRouter();
 
-export default Onboarding
+   useEffect(() => {
+    console.log("Onboarding Page Org :", organization)
+    
+    if (organization) {
+      router.push(`/organization/${organization.slug}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [organization]);
+
+	return (
+		<div className="flex justify-center items-center pt-14">
+			<OrganizationList hidePersonal />
+		</div>
+	);
+};
+
+export default Onboarding;
